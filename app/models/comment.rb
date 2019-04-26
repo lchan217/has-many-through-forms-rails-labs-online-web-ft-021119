302dc3058d285211ae203316bf563555eb275fd6
Lizzie <lchan217@gmail.com>
2019-04-26 08:37:26 -0500

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,15 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-
+  def user_attributes=(user_attributes)
+    user_attributes.each do |i, attr|
+      if user_attributes.comments.present? #prevents user being created if there's no comment
+        user = User.find_or_create_by(attr)
+                #user = User.find_or_create_by(username: attr[:username])
+        if !self.users.include?(user)
+          self.comments.build(user: user) #associates
+        end
+      end
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -16,17 +16,7 @@ class Post < ActiveRecord::Base
     end
   end
 
-  def user_attributes=(user_attributes)
-    user_attributes.each do |i, attr|
-      if user_attributes.comments.present? #prevents user being created if there's no comment
-        user = User.find_or_create_by(attr)
-                #user = User.find_or_create_by(username: attr[:username])
-        if !self.users.include?(user)
-          self.comments.build(user: user) #associates
-        end
-      end
-    end
-  end
+
 
   # def comments_attributes=(comment_attributes)
   #   comment_attributes.each do |i, attr|
